16ca5283cbe97d92893e4a18170ad30cbe8db1d8
0utw1t <albertobadillo27@gmail.com>
2016-04-10 18:38:44 -0500

    ProyectosPersonales

---
+++ BuscaArchivos.ps1
@@ -0,0 +1,61 @@
+﻿    #Hecho por Mario Alberto Sanchez Badillo
+    #Oficina de seguridad KIONETWORKS
+Function Search-Files{
+    
+    <#
+      .SYNOPSIS
+      Busca Archivos
+
+      .DESCRIPTION
+
+      Funcion que nos apoya en la busqueda de archivos, por fecha de inicio y fin; en una
+      determinada Ruta que como usuario le asignemos.
+      .EXAMPLE
+
+      Search-Files -Inicio 02/20/2016 -Fin 02/26/2016 -Ruta C:\Users\Usuario\Desktop -Resultados C:\Users\Usuario\Desktop\Resultados
+    #>
+
+
+    [CmdletBinding()]
+
+    Param(
+    [Parameter(Mandatory=$True,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]
+    [datetime]$Inicio,
+
+    [Parameter(Mandatory=$True,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]
+    [datetime]$Fin,
+    
+    
+    [Parameter(Mandatory=$True,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]
+    $Ruta,
+
+    [Parameter(Mandatory=$True,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]
+    $Resultados,
+
+    [Parameter(Mandatory=$false,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]
+    [String]$Extencion
+     )
+
+     Begin{}
+
+     Process{
+     Get-ChildItem -Path $Ruta $Extencion -Recurse -Force  | where {($_.LastWriteTime -gt $Inicio) -and ($_.LastWriteTime -lt $Fin)} | Out-File -Width 255 $Resultados
+     $Directorios =  Get-ChildItem -Path $Ruta -Recurse -Force $Extencion | where {($_.LastWriteTime -gt $Inicio) -and ($_.LastWriteTime -lt $Fin)} 
+     $Directorios.DirectoryName
+     }
+
+     End{
+        notepad.exe $Resultados
+     }
+    
+}
---
+++ ParseNmap.ps1
@@ -0,0 +1,39 @@
+﻿function Nmap-Report
+{
+Param(
+    $Ruta
+)
+
+Begin{}
+
+Process{    
+
+[xml]$Nmap = Get-Content $Ruta
+
+$discoveredhosts = $nmap.nmaprun.host | Where-Object {$_.status.state -eq "up"}
+
+foreach($hosts in $discoveredhosts)
+    {
+    foreach ($address in $hosts.address | ForEach-Object {if($_.addrtype -eq "ipv4"){$_.addr}})
+        {
+        Write-Host "----------------------------------------------------------------------------"
+        Write-Host "Remote Hosts" $address
+            Write-Host ""
+            foreach($os in $hosts.os.osmatch | Format-Table Name,accuracy -AutoSize)
+            {
+                $os
+            }
+            foreach($ports in $hosts.ports.port)
+            {
+                $ports | Where-Object {$_.state.state -contains "open"} |
+                Select-Object -Property Protocol,PortId, @{Name="Status"; Expression={$_.state.state}},@{Name="Servicio"; Expression={$_.service.name}}
+
+            } 
+    
+        }
+    }
+}
+
+End{}
+
+} 
