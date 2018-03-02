8ab480f7a539b2685ecd34f210b5a6cb3d2e54af
kcah27 <albertobadillo27@gmail.com>
2018-03-01 18:42:05 -0600

    Add files via upload

---
+++ Search-SubDomains.ps1
@@ -0,0 +1,412 @@
+﻿Function Search-SubDomains{
+    <#
+
+    .SYNOPSIS
+        Script que automatiza la busqueda de subdominios en fuentes publicas
+
+    .DESCRIPTION
+        Script que automatiza la busqueda de subdominios en fuentes publicas como:
+        www.google.com, www.bing.com, www.virustotal sin la necesidad de un APIKEY
+
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -google
+        Busqueda de subdominios en www.google.com
+    
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -bing
+        Busqueda de subdominios en el buscador de Microsoft Bing
+
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -virustotal
+        Busqueda de subdominios en www.VirusTotal.com
+        Warning: Solo acepta 3 busquedas, ya que se realiza sin APIKEY
+
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -all
+        Busqueda en Google, Bing y VirusTotal
+
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -google -bing
+        Busqueda multiple
+
+    .EXAMPLE
+        Search-SubDomains -D "dominio" -google -bing | Out-GridView
+        Search-SubDomains -D "dominio" -bing | Out-File -FilePath
+        Search-SubDomains -D "dominio" -virustotal | Export-Csv -Path
+        $variable = Search-SubDomains -D "dominio" -all
+        Salida en multiples formatos y variables
+    
+    .NOTES
+        Twitter: @kcah27
+        mail: albertokcah27@outlook.com
+    #>
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [string[]]$D,
+        [switch]$google,
+        [switch]$bing,
+        [switch]$virustotal,
+        [switch]$all
+    )
+
+    Begin{
+
+        #Variables Globales
+        $Global:UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
+
+        #Funciones
+        Function Search-Google{
+
+        param(
+
+            [string]$domain
+
+        )
+
+        Begin{
+        
+            $dominio = $domain
+            $www = "www."+$dominio
+            $MaxSearchPages = 5
+            $googlesearch = "https://www.google.com/search?q=site:$dominio+-$www&num=100"
+            $pagelinks = (Invoke-WebRequest -Uri $googlesearch -UserAgent $UserAgent -UseBasicParsing).Links
+            $validlinks = @()
+            $subdomains = @()
+            $otherpages = @()
+            $hrefs = @()
+            $otherpageslimit = @()
+            $morepagelinks = @()
+            $morehrefs = @()
+        }
+
+        Process{
+
+            foreach($links in $pagelinks)
+            {
+                $hrefs += $links.href
+            }
+            foreach ($url in $hrefs)
+            {
+                if(($url -like "*$dominio*") -and ($url -notlike "*https://www.google.com*") -and ($url -notlike "https://maps.google.com*") -and ($url -notlike "http://webcache.googleusercontent.com*") -and ($url -notlike "https://webcache.googleusercontent.com*") -and ($url -notlike "https://translate.google.com*"))
+                {
+                    if($url -like "*http://*")
+                    {
+                        $strippedurl = [regex]::Match($url,"http([^\)]+)").value
+                        $validlinks += $strippedurl 
+                    }
+                    elseif($url -like "*https://*")
+                    {
+                        $strippedurl = [regex]::Match($url,"https([^\)]+)").value
+                        $validlinks += $strippedurl 
+                    }
+                }
+            }
+
+        
+            foreach($url in $hrefs) 
+            {
+                if ($url -like "*search?q*start=*")
+                {
+                    $otherpages += "https://www.google.com$url" + "&num=100"
+                }
+            }
+
+            $otherpages = $otherpages | sort | unique
+            $pagecount = $otherpages.count
+
+            if ($pagecount -gt $MaxSearchPages)
+            {
+                $totalpagelimit = $MaxSearchPages
+                for($j=0; $j -lt $totalpagelimit; $j++)
+                {                                    
+                    $otherpageslimit += $otherpages[$j]
+                }
+            }
+
+            $otherpageslimit = $otherpageslimit -replace "&amp;","&"
+            foreach($page in $otherpageslimit)
+            {
+                $i++
+                $morepagelinks = (Invoke-WebRequest -Uri $page -UserAgent $UserAgent -UseBasicParsing).Links
+                $morehrefs = $morepagelinks.href
+                foreach($url in $morehrefs)
+                {
+                    if (($url -like "*$dominio*"))
+                    {
+                        if ($url -like "*http://*")
+                        {
+                            $strippedurl = [regex]::match($url,"https([^\)]+)").Value
+                            $validlinks += $strippedurl
+                        }
+                        elseif ($url -like "*https://*")
+                        {
+                            $strippedurl = [regex]::match($url,"https([^\)]+)").Value
+                            $validlinks += $strippedurl
+                        }
+                    }     
+                }
+            }
+        }
+        End{
+            foreach ($valid in $validlinks)
+            {
+                $subdomain = [regex]::Split($valid,"\/")[2]
+                $subdomains += $subdomain
+            }
+            #$subdomains | select -Unique
+
+            Set-Variable -Name resultgoogle -Value $subdomains -Scope Global
+        }
+
+    }#Fin funcion Google
+        
+        Function Search-Bing{
+    
+        param(
+        
+            [string]$domain
+
+        )
+
+        Begin{
+
+            $dominio = $domain
+            $www = "www."+$dominio
+            $bingsearch = "http://www.bing.com/search?q=site:$dominio&count=30"
+            $bingpagelinks = @()
+            $bingpagelinks = (Invoke-WebRequest -Uri $bingsearch -UserAgent $UserAgent -UseBasicParsing).Links
+            $binghrefs = @()
+            $bingotherpages = @()
+            $morepagelinks = @()
+            $morehrefs = @()
+            $validlinks = @()
+            $subdomains = @()
+
+        }
+
+        Process{
+
+            foreach($links in $bingpagelinks)
+            {
+                $binghrefs += $links.href
+            }
+
+            foreach($url in $binghrefs)
+            {
+                if($url -like "*$dominio*")
+                {
+                    if($url -like "*http://*")
+                    {
+                        $strippedurl = [regex]::Match($url,"http([^\)]+)").value
+                        $validlinks += $strippedurl 
+                    }
+                    elseif($url -like "*https://*")
+                    {
+                        $strippedurl = [regex]::Match($url,"https([^\)]+)").value
+                        $validlinks += $strippedurl 
+                    }
+                }
+            }
+                
+
+            foreach($url in $binghrefs)
+            {
+                if ($url -like "*search?q*first=*")
+                {
+                    $bingotherpages += "https://www.bing.com$url" + "&count=30"
+                }
+            }
+                
+            $bingotherpages = $bingotherpages | sort | unique
+            $bingotherpages = $bingotherpages -replace "&amp;","&"
+            foreach($page in $bingotherpages)
+            {
+                $morepagelinks = (Invoke-WebRequest -Uri $page -UserAgent $UserAgent -UseBasicParsing).Links
+                $morehrefs = $morepagelinks.href
+                foreach($url in $morehrefs)
+                {
+                    if (($url -like "*$dominio*"))
+                    {
+                        if ($url -like "*http://*")
+                        {
+                            $strippedurl = [regex]::Match($url,"http([^\)]+)").value
+                            $validlinks += $strippedurl
+                        }
+                        elseif ($url -like "*https://*")
+                        {
+                            $strippedurl = [regex]::Match($url,"https([^\)]+)").value
+                            $validlinks += $strippedurl
+                        }
+                    }
+                }
+            }
+        }#Fin Process
+
+        End{
+        
+            foreach ($valid in $validlinks)
+            {
+                $subdomain = [regex]::Split($valid,"\/")[2]
+                $subdomains += $subdomain
+            }
+            #$subdomains | select -Unique
+
+            Set-Variable -Name resultbing -Value $subdomains -Scope Global
+        }
+
+    }#Fin funcion Bing
+        
+        Function Search-VirusTotal{
+    
+        param(
+
+            [string]$domain
+
+        )
+
+        Begin{
+
+            $dominio = $domain
+            $uri = "https://www.virustotal.com/en/domain/" + $dominio +"/information/"
+            $UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
+            $response = Invoke-WebRequest -Uri $uri -Method Get -UserAgent $UserAgent
+            $term = $dominio.Split(".")
+            $term = $term[1].ToString()
+            $term = "." + "$term"
+            $hrefs = @()
+            $validlinks = @()
+            $subdomains = @()
+        }
+
+        Process{
+        
+            foreach($links in $response.Links)
+            {
+                $hrefs += ($links.href)
+            }
+
+            foreach($url in $hrefs)
+            {
+                if($url -like "*$dominio*")
+                {
+                    if($url -like "*$term*")
+                    {
+                        $validlinks += $url.Replace('/information/','').Replace('/ca/domain/','').Replace('/en/domain/','').Replace('/da/domain/','').Replace('/de/domain/','').Replace('/es/domain/','').Replace('/fr/domain/','').Replace('/hr/domain/','').Replace('/it/domain/','').Replace('/hu/domain/','').Replace('/nl/domain/','').Replace('/nb/domain/','').Replace('/pt/domain/','').Replace('/pl/domain/','').Replace('/sk/domain/','').Replace('/uk/domain/','').Replace('/vi/domain/','').Replace('/tr/domain/','').Replace('/ru/domain/','').Replace('/sr/domain/','').Replace('/bg/domain/','').Replace('/he/domain/','').Replace('/ka/domain/','').Replace('/ar/domain/','').Replace('/fa/domain/','').Replace('/zh-cn/domain/','').Replace('/zh-tw/domain/','').Replace('/ja/domain/','').Replace('/ko/domain/','')     
+                    }
+                }
+            }
+
+
+
+        }#fin Process
+    
+        End{
+            #$validlinks | select -Unique
+            
+            foreach($l in $validlinks){
+                $subdomains += $l
+            }
+
+            Set-Variable -Name resultvt -Value $subdomains -Scope Global
+        }
+
+    } #Fin funcion Virus Total
+
+    }#Fin Begin Funcion principal
+
+    Process{
+        if(($google -eq $false) -and ($bing -eq $false) -and ($virustotal -eq $false) -and ($all -eq $false)){
+            Write-Host "Favor de indicar parametro de busqueda" -ForegroundColor Red
+        }
+
+        if($google){
+            Search-Google -domain $D
+            $resultgoogle | Select -Unique
+        }
+
+        elseif($bing){
+            Search-Bing -domain $D
+            $resultbing | Select -Unique
+        }
+    
+        elseif($virustotal){
+            Search-VirusTotal -domain $D
+            $resultvt | Select -Unique
+        }
+        elseif(($all) -or (($google -eq $true) -and ($bing -eq $true) -and ($virustotal -eq $true))){
+            Search-Google -domain $D
+            Search-Bing -domain $D
+            Search-VirusTotal -domain $D
+            
+            $resultall = @()
+            foreach($z in $resultgoogle)
+            {
+                $resultall +=$z
+            }
+            
+            foreach($z in $resultbing)
+            {
+                $resultall +=$z
+            }
+            
+            foreach($z in $resultvt)
+            {
+                $resultall +=$z
+            }
+            $resultall | select -Unique
+        }
+        elseif(($google -eq $true) -and ($bing -eq $true)){
+            Search-Google -domain $D
+            Search-Bing -domain $D
+            $resultall = @()
+            foreach($z in $resultgoogle)
+            {
+                $resultall +=$z
+            }
+            
+            foreach($z in $resultbing)
+            {
+                $resultall +=$z
+            }
+            $resultall | select -Unique
+        }
+        elseif(($google -eq $true) -and ($virustotal -eq $true)){
+            Search-Google -domain $D
+            Search-VirusTotal -domain $D
+            $resultall = @()
+            foreach($z in $resultgoogle)
+            {
+                $resultall +=$z
+            }
+
+            foreach($z in $resultvt)
+            {
+                $resultall +=$z
+            }
+            $resultall | select -Unique
+
+        }
+        elseif(($bing -eq $true) -and ($virustotal -eq $true)){
+            Search-Bing -domain $D
+            Search-VirusTotal -domain $D
+            $resultall = @()
+            foreach($z in $resultbing)
+            {
+                $resultall +=$z
+            }
+            
+            foreach($z in $resultvt)
+            {
+                $resultall +=$z
+            }
+            $resultall | select -Unique
+        }
+
+    }#Fin Process Funcion principal
+
+    End{
+    
+    }#End de la Funcion principal
+
+}
---
+++ nsearch.ps1
@@ -0,0 +1,279 @@
+﻿
+#Instalamos Nsearch
+if(Test-Path -Path 'C:\Nsearch')
+{
+    if(Test-Path -Path 'C:\Nsearch\PSSQLite')
+    {
+        if(Test-Path -Path 'C:\Nsearch\db')
+        {
+        }
+    }
+}
+else
+{
+    [string]$location = (Get-Location).Path
+    $location = "$location" + "\*"
+    New-Item -Path 'C:\' -Name Nsearch -ItemType Directory | Out-Null
+    Copy-Item -Path $location -Destination 'C:\Nsearch' -Force
+}
+
+#Verificamos si Nmap esta instalado
+$envpathnmap = $env:Path.Split(";")
+$envpathnmap = $envpathnmap | Select-String -Pattern "Nmap$"
+if(Test-Path -Path "$envpathnmap")
+{
+    Clear-Host
+}
+else
+{
+    Write-Host "No se encontro Nmap" -ForegroundColor Red -BackgroundColor Black
+    switch($opc){
+        1{
+            $global:envpathnmap = Read-Host -Prompt "Path Nmap"    
+        }
+        2{
+            Write-Host "Download Nmap and WinPcap" -ForegroundColor Green
+            $download = New-Object System.Net.WebClient    
+            $a.DownloadFile("https://nmap.org/dist/nmap-7.40-setup.exe","C:\test\nmap-7.40-setup.exe")
+            $a.DownloadFile("https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe","C:\test\WinPcap_4_1_3.exe")
+            Write-Host "Descargos" -ForegroundColor Green
+            Write-Host
+        }
+    }
+    
+    
+    
+}
+
+
+Write-Host "========================================"
+Write-Host " _   _  _____                     _     "
+Write-Host "| \ | |/ ____|                   | |    "
+Write-Host "|  \| | (___   ___  __ _ _ __ ___| |__  "
+Write-Host "| . ` |\___ \ / _ \/ _` | '__/ __| '_ \ "
+Write-Host "| |\  |____) |  __/ (_| | | | (__| | | |"
+Write-Host "|_| \_|_____/ \___|\__,_|_|  \___|_| |_|"
+Write-Host "========================================"
+Write-Host "Version 0.1              @kcah27"
+Write-Host ""
+
+#Funcion para cambiar el Prompt
+Function Global:prompt{
+        Write-Host ("NS>") -nonewline -foregroundcolor Magenta
+        return " "
+    }
+
+Function ayuda{
+    param(
+        [switch]$search,
+        [switch]$doc,
+        [switch]$templates,
+        [switch]$run,
+        [switch]$update
+    )
+
+    Begin{}
+    
+    Process{
+
+        if($search)
+        {
+            Write-Host ""
+            Write-Host "Use Example: " -ForegroundColor Cyan
+            Write-Host "search -name http" -ForegroundColor Green
+            Write-Host "search -category ex..[autocomplete]" -ForegroundColor Green
+            Write-Host "search -author Pau..[autocomplete]" -ForegroundColor Green
+        }
+        elseif($doc)
+        {
+            Write-Host ""
+            Write-Host "Use Example: " -ForegroundColor Cyan
+            Write-Host "doc -name ht..[autocomplete]"  -ForegroundColor Green
+        }
+        elseif($templates){
+            Write-Host ""
+            Write-Host "Use Example: " -ForegroundColor Cyan
+            Write-Host "Template" -ForegroundColor Green
+        }
+        elseif($run)
+        {
+            Write-Host ""
+            Write-Host "Use Example: " -ForegroundColor Cyan
+            Write-Host "run" -ForegroundColor Green
+        }
+        elseif($update)
+        {
+            Write-Host ""            
+            Write-Host "Update nmap scripts" -ForegroundColor Green
+        }
+        else{
+            Write-Host ""
+            Write-Host "Comandos disponibles: " -ForegroundColor Cyan
+            Write-Host "search     doc     templates" -ForegroundColor Green
+            Write-Host "run        update" -ForegroundColor Green
+            Write-Host ""   
+        }
+    }
+    End{}    
+}
+
+Function search{
+    
+    param(
+        [string]$nombre,
+        [ValidateSet("auth","broadcast","brute","default","discovery","dos","exploit","external","fuzzer","intrusive","malware","safe","version","vuln")]
+        [string]$categoria,
+        [string]$autor
+    )
+
+    Begin{
+        $envpathnmap = $env:Path.Split(";")
+        $envpathnmap = $envpathnmap | Select-String -Pattern "Nmap$"
+        $envpathnmap = $envpathnmap.ToString() + "\scripts"
+        $db = Get-Content -Path ("$envpathnmap" + "\script.db")    
+    }
+    
+    Process{
+        foreach($line in $db)
+        {
+            $line = $line.replace('Entry { filename = "',"").replace('", categories = { "',',"').replace('", } }','"').replace('", "','","')
+            $line = $line.Split(",")
+    
+            $pathdoc = ("$envpathnmap" + "\" + "$($line[0])")
+            $doc = Get-Content -Path $pathdoc
+            
+            foreach($l in $doc)
+            {
+                if($l.StartsWith("author"))
+                    {
+                        $author = $l.replace('author = "',"").replace('"',',"').replace('[[',"").replace(',"',"").replace("author =","Brandon Enright <bmenrigh@ucsd.edu>, Duane Wessels <wessels@dns-oarc.net>")    
+                        #$author
+                    }
+            }
+            if(($line[0] -match "$nombre") -and ($line[0] -match "$categoria") -and ($author -match "$autor"))
+            {
+                $line[0]
+            }
+        }
+    
+    }
+
+    End{}
+
+}
+
+Function doc{
+    [CmdletBinding()]
+    param()
+    DynamicParam {
+        $envpathnmap = $env:Path.Split(";")
+        $envpathnmap = $envpathnmap | Select-String -Pattern "Nmap$"
+        $envpathnmap = $envpathnmap.ToString() + "\scripts"
+        
+        # Set the dynamic parameters' name
+        $ParameterName = 'namescript'
+            
+        # Create the dictionary 
+        $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        # Create the collection of attributes
+        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+            
+        # Create and set the parameters' attributes
+        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+        $ParameterAttribute.Mandatory = $true
+        $ParameterAttribute.Position = 1
+
+        # Add the attributes to the attributes collection
+        $AttributeCollection.Add($ParameterAttribute)
+
+        # Generate and set the ValidateSet 
+        $arrSet = Get-ChildItem -Path $envpathnmap | Select-Object -ExpandProperty Name
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+
+        # Add the ValidateSet to the attributes collection
+        $AttributeCollection.Add($ValidateSetAttribute)
+
+        # Create and return the dynamic parameter
+        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+        $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
+        return $RuntimeParameterDictionary
+    }
+
+    Begin{
+        $namescript = $PsBoundParameters[$ParameterName]
+        $pathscript = "$envpathnmap" +"\" + "$namescript"
+    }
+
+    Process{
+        $script = Get-Content -Path "$pathscript"
+        foreach ($line in $script)
+        {
+            if($line.StartsWith("license"))
+            {
+                break
+            }
+        Write-Host $line -ForegroundColor Green
+        }
+    }
+
+    End{}
+
+}
+
+Function templates{
+
+    Begin{
+    
+    }
+
+    Process{
+    
+    }
+
+    End{
+    
+    }
+}
+
+Function update{
+    Begin{}
+    Process{
+        if(([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+        {
+            nmap --script-updatedb
+        }
+        else
+        {
+            Write-Host "Please run this script with admin priviliges" -ForegroundColor Red -BackgroundColor Black
+        }
+    }
+    End{}
+}
+
+#Funcion para regresar el prompt original
+Function quit{
+            
+    Begin{
+        #Clear-Host
+    }
+
+    Process
+    {
+        Function Global:prompt{"PS " + "$(Get-Location)" + "> "}
+    }
+
+    End{
+        Write-Host "Saliendo Nsearch :D ..." -ForegroundColor Yellow
+        Start-Sleep 2
+        Clear-Host
+
+        #Removemos las funciones construidas
+        Remove-Item -Path Function:\ayuda
+        Remove-Item -Path Function:\search
+        Remove-Item -Path Function:\doc
+        Remove-Item -Path Function:\update
+        Remove-Item -Path Function:\quit
+    }
+}
+
